65916b41edb538e6e780b8e58dcb841909225e10
Mike HAPPI <mikehappi@me.com>
2019-05-16 23:25:56 +0800

    Update header.php

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
                             <?php
                                                 wp_nav_menu( array(
                                                     'theme_location' => 'menu-1',
-                                                    'menu_id'        => 'primary-menu',
+                                                    'menu_id'        => '17',
                                                 ) );
                              ?>
 
