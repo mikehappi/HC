ba26c9870d885a4529d0969c5f7d02705f73f63a
Mike HAPPI <mikehappi@me.com>
2019-05-16 12:14:15 +0800

    Update landing.php

--- template-parts/landing.php
+++ template-parts/landing.php
@@ -38,11 +38,11 @@ height: auto !important;}
 
 
 .landing .section.p81 {
-padding:10% 0% 0% 15%!important;}
+padding:10% 0% 0% 12%!important;}
 .landing .section.p113 {
-padding:10% 0% 0% 15%!important;}
+padding:10% 0% 0% 12%!important;}
 .landing .section.p97 {
-padding:10% 0% 0% 15% !important;}
+padding:10% 0% 0% 12% !important;}
 
 .section.p81 .lcontainer {
   display: inline-block;
