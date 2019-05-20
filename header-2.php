2e9d364bdc1f6207ef2f62039f5556077982836a
Mike HAPPI <mikehappi@me.com>
2019-05-20 22:38:30 +0800

    Update header-2.php

--- header-2.php
+++ header-2.php
@@ -204,7 +204,7 @@ window.onclick = function(event) {
 <script>
 window.onscroll = function (event)
 {
-    if(this.scrollY = 3510)
+    if(this.scrollY > 3510)
     {
         alert('3510');
     }
