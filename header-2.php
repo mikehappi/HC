efb7549e5b60869398af04e27f10124055ad983a
Mike HAPPI <mikehappi@me.com>
2019-05-20 11:24:18 +0800

    scrolled

--- header-2.php
+++ header-2.php
@@ -118,7 +118,23 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+<script>
+<script>
+$(function() {
+    var header = $(".navbar-default");
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            header.addClass("scrolled");
+        } else {
+            header.removeClass("scrolled");
+        }
+    });
+
+});
 </script>
+
         </nav>
 
 
