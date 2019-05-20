7a3a9cc5bf831e0449264d7a087ff351c068d00d
Mike HAPPI <mikehappi@me.com>
2019-05-20 21:52:28 +0800

    Update header-2.php

--- header-2.php
+++ header-2.php
@@ -197,7 +197,7 @@ window.onclick = function(event) {
 <script>
   window.onscroll = () => {
   const nav = document.querySelector('#nav');
-  if(this.scrollY >=684) nav.className = 'scroll';
+  if(this.scrollY >=735) nav.className = 'scroll';
     else if(this.scrollY >1800) nav.className = 'scroll2';
   else nav.className = 'navbar-default';
 };
