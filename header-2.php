f0eb686046b79ed26a7bae8366524111791b724d
Mike HAPPI <mikehappi@me.com>
2019-05-20 12:05:41 +0800

    scroll

--- header-2.php
+++ header-2.php
@@ -137,7 +137,7 @@ $(function() {
 <script>
   window.onscroll = () => {
   const nav = document.querySelector('#nav');
-  if(this.scrollY <= 50) nav.className = ''; else nav.className = 'scroll';
+  if(this.scrollY >= 50) nav.className = 'scroll'; else nav.className = '';
 };
 </script>
 
