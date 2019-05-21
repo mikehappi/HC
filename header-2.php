b88f80083d45feeb8e16a33af2839f595878ac50
Mike HAPPI <mikehappi@me.com>
2019-05-21 13:01:07 +0800

    menu

--- header-2.php
+++ header-2.php
@@ -199,7 +199,7 @@ window.onclick = function(event) {
 var go = true;
   window.onscroll = () => {
   const nav = document.querySelector('#nav');
-  if(this.scrollY <700 || this.scrollY>3500){nav.className = 'navbar-default';}
+  if(this.scrollY <698 || this.scrollY>3498){nav.className = 'navbar-default';}
   else {nav.className = 'scroll';}
 };
 </script>
