4e50c7508bb4d95604e46c64213e7b82170ee26d
Mike HAPPI <mikehappi@me.com>
2019-05-22 21:34:14 +0800

    Update landing.php

--- template-parts/landing.php
+++ template-parts/landing.php
@@ -154,7 +154,7 @@ padding:10% 0% 0% 12% !important;}*/
 </div>
 
 
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+ <script>
  /*!
   * jQuery Scrollify
   * Version 1.0.20
