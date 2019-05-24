c65b37b8b3928857188dd587720b5eab8f160977
Mike HAPPI <mikehappi@me.com>
2019-05-24 14:02:14 +0800

    Update header-2.php

--- header-2.php
+++ header-2.php
@@ -193,9 +193,9 @@ var go = true;
   window.onscroll = () => {
   const nav = document.querySelector('#nav');
   // When the user scrolls down 695px from the top of the document, show the button
-  if(this.scrollY <695){nav.className = 'navbar-default';document.getElementById("topBtn").style.display = "none";document.getElementById("topBtnB").style.display = "none";}
-  else if(this.scrollY>3495){nav.className = 'navbar-default';document.getElementById("topBtn").style.display = "block";document.getElementById("topBtnB").style.display = "none";}
-  else {nav.className = 'scroll'; nav.className = 'navbar-default';document.getElementById("topBtnB").style.display = "block";document.getElementById("topBtn").style.display = "none";}
+  if(this.scrollY <695){nav.className = 'navbar-default';document.getElementById("topBtn").style.display = "none";}
+  else if(this.scrollY>3495){nav.className = 'navbar-default';}
+  else {nav.className = 'scroll';}
 };
 
 // When the user clicks on the button, scroll to the top of the document
