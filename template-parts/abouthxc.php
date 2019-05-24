209fdfd592a12ebfdfb23305bf820e3d5770f2c8
Mike HAPPI <mikehappi@me.com>
2019-05-24 16:29:07 +0800

    Update abouthxc.php

--- template-parts/abouthxc.php
+++ template-parts/abouthxc.php
@@ -7,7 +7,9 @@
 <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
 <?php $upload_dir = wp_upload_dir(); ?>
  <!doctype html>
-    <html <?php language_attributes(); ?>>
+    <html <?php language_attributes(); ?>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
    <meta name="viewport" content="width=device-width,initial-scale=1">
 <?php
 /**
@@ -124,7 +126,7 @@ text-transform:uppercase !important;
 z-index: -1;
 }
 .fired{
-  width: 45%;
+  width: 540px;
     float: left;
     position: relative;
   right: -5em; /* or left 30% */
