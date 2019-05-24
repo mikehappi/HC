5b60f56e5546b7be4a1fe5045ce5713d954eb111
Mike HAPPI <mikehappi@me.com>
2019-05-24 13:17:10 +0800

    js

--- header-2.php
+++ header-2.php
@@ -188,10 +188,26 @@ window.onclick = function(event) {
   }
 }
 </script>
+<script>
+var go = true;
+  window.onscroll = () => {
+  const nav = document.querySelector('#nav');
+  // When the user scrolls down 695px from the top of the document, show the button
+  if(this.scrollY <695){nav.className = 'navbar-default';document.getElementById("topBtn").style.display = "none"}
+  else if(this.scrollY>3495){nav.className = 'navbar-default';}
+  else {nav.className = 'scroll';
+  document.getElementById("topBtn").style.display = "block"}
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+</script>
 
 
-
-        </nav>
+</nav>
 
 
 </head>
--- template-parts/landing.php
+++ template-parts/landing.php
@@ -164,25 +164,8 @@ padding:10% 0% 0% 15% !important;}
 </div>
 
 <!--go to the top bottom -->
- <div class="row text-right hidden-xs hidden-md hidden-sm">
+<div class="row text-right hidden-xs hidden-md hidden-sm">
 <button onclick="topFunction()" id="topBtn" title="Go to top" type="image" style="border: 0; background: transparent"><svg xmlns="http://www.w3.org/2000/svg" width="15" height="18"><path d="M6.862 18h1.276V3.761c0-.5 0-.898-.049-1.682.503.58 1.13 1.17 1.633 1.637l4.37 4.056.908-.84L7.5 0 0 6.931l.908.841 4.37-4.056c.504-.466 1.13-1.057 1.633-1.637-.05.784-.05 1.182-.05 1.682V18z" fill="#e5e5e4"></path></svg>
 </button>
- </div>
-
-<script>
-var go = true;
-  window.onscroll = () => {
-  const nav = document.querySelector('#nav');
-  if(this.scrollY <695){nav.className = 'navbar-default';document.getElementById("topBtn").style.display = "none"}
-  else if(this.scrollY>3495){nav.className = 'navbar-default';}
-  else {nav.className = 'scroll';
-  document.getElementById("topBtn").style.display = "block"}
-};
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-</script>
+</div>
 <?php get_footer(); ?>
