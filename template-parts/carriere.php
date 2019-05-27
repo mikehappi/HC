5ece1f76e3799fb50392b10ab1901817b977a694
Mike HAPPI <mikehappi@me.com>
2019-05-28 02:24:48 +0800

    Update carriere.php

--- template-parts/carriere.php
+++ template-parts/carriere.php
@@ -25,7 +25,7 @@
      src:url("<?php echo get_template_directory_uri(); ?>/fonts/LucidaFax-Regular.ttf");
 }
  @font-face{
-    font-family:SAVOYELFD;
+    font-family:LFD;
     src:url("<?php echo get_template_directory_uri(); ?>/fonts/LucidaFax-Demibold.ttf");
 }
   body {
@@ -66,10 +66,10 @@ position: sticky !important;
 }
 
 
- /* All link will be red HX */
- a {
-color: #b91338 !important;
-}
+ /* All link will be red HX  a {
+ color: #b91338 !important;
+ }*/
+
 
 body, html {
 height: 100%;
