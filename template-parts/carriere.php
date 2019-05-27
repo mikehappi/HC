50c62224e22bb39c9bec90013f9f6f17b253913f
Mike HAPPI <mikehappi@me.com>
2019-05-27 23:10:42 +0800

    Update carriere.php

--- template-parts/carriere.php
+++ template-parts/carriere.php
@@ -1,12 +1,19 @@
 <?php
 /**
- * Template Name:Carriere Page
+ * Template Name:About HC Page
  */
 
 ?>
 <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
 <?php $upload_dir = wp_upload_dir(); ?>
-  <meta name="viewport" content="width=device-width,initial-scale=1">
+<!doctype html>
+<html <?php language_attributes(); ?>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<?php
+/**
+ * Move the styles below into your stylesheet
+ */
+?>
 <style type="text/css">
 
 @font-face{
@@ -18,7 +25,7 @@
      src:url("<?php echo get_template_directory_uri(); ?>/fonts/LucidaFax-Regular.ttf");
 }
  @font-face{
-    font-family:LFD;
+    font-family:SAVOYELFD;
     src:url("<?php echo get_template_directory_uri(); ?>/fonts/LucidaFax-Demibold.ttf");
 }
   body {
@@ -31,23 +38,11 @@
 text-transform:uppercase !important;
 text-align: center;
 font-family:LFR !important;
-padding-top: 5em;
+/*padding-top: 5em;*/
 color:#383838!important;
 font-size:15px !important;
-line-height: 2em;
-
-}
- /* Put down hearder Nicely */
- .hd .navbar-default {
- position: sticky !important;
- }
-
-
-body, html {
-    height: 100%;
-    margin: 0;
- color: black !IMPORTANT;
-
+position: relative;
+top: 10%
 }
 .navbar-default .myBtn a::before {
     content: '';
@@ -64,20 +59,92 @@ body, html {
 .navbar-default a {
     color: white !important;
 }
+
+/* Padding Header */
+.hd .navbar-default {
+position: sticky !important;
+}
+
+
+ /* All link will be red HX */
+ a {
+color: #b91338 !important;
+}
+
+body, html {
+height: 100%;
+margin: 0;
+color: black !IMPORTANT;
+
+}
+
 .bg {
-    /* The image used  for Background */
+    /* The image used  */
 
     background: url('<?php echo $backgroundImg[0]; ?>') no-repeat;
     /* Full height */
     width: 100% !important;
     height: 40%!important;
+
     /* Center and scale the image nicely */
     background-position: center;
     background-repeat: no-repeat;
     background-size: 100% 100%;
 }
+
+
 /*--------------------------------------------------------------
 # Content
+--------------------------------------------------------------*/
+.fired {
+font-family:LFD !important;
+color: #b91338!important;
+font-size:20px !important;
+text-transform:uppercase !important;
+}
+.fired p {
+
+  margin-top: 0px!important;
+  /*margin-bottom: 0px !important;*/
+
+}
+
+.fitext {
+font-family:PNR !important;
+color: #383838!important;
+font-size:14px !important;
+text-transform:uppercase !important;
+}
+
+.ficontent {
+  width: 100%;
+  text-align: center;
+  padding: 3em 0 3em 0;
+  position: relative;
+z-index: -1;
+}
+.fired{
+  width: 565px;
+  display: inline-block;
+  text-align: left;
+  padding: 30px;
+  margin: 15px;
+  vertical-align: top;
+}
+.fired img{
+  width:380px !important;
+  height:3px !important;
+  float: left;
+}
+.fitext {
+width:630px;
+text-align: justify;
+display: inline-block;
+padding: 30px;
+margin: 15px;
+}
+/*--------------------------------------------------------------
+# Content carriere
 --------------------------------------------------------------*/
  /* carriere page apply font and other*/
 .carriere{
@@ -88,16 +155,8 @@ font-family:LFR !important;
 font-weight: 600;
 display:inline-block;
 color:#383838 !important;
+line-height: 2em;
 }
-
-.cacontent      {width: 100%;
-    /* float: none; */
-    padding: 4em;
-    /* display: inline-block; */
-    z-index: -1;
-    position: relative;
-    text-align: center;
-  /* padding: 15px; */}
 
 /*--------------------------------------------------------------
 # Media Qeries  Mobile CSSS adapation
@@ -107,15 +166,14 @@ color:#383838 !important;
 }
 
 </style>
-<body>
-<!--display  banner x Menu -->
+<!--display  banner x Menu RED WHITE -->
 <div class="bg">
 <div class="hd"><?php get_header(2); ?></div>
-			<header class="entry-header">
-				<h1 class="page-title"><?php the_title(); ?></h1>
-			</header>
+<header class="entry-header">
+		<h1 class="page-title"><?php the_title(); ?></h1>
+</header>
 </div>
-<div class="cacontent">
+<div class="ficontent">
 <?php
 if (have_posts()):
   while (have_posts()) : the_post();
